e132c1a7dd3cc98c97507699f2d7dd2827924169
alan <alanarostegui@hotmail.com>
2021-04-27 14:55:08 -0500

    add unusual pdf drop component

---
+++ components/Shared/PdfDropDiv.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Container, Divider, Icon } from "semantic-ui-react";
+
+export default function Home() {
+  const [media, setMedia] = useState(null);
+  const [extension, setExtension] = useState("");
+
+  return (
+    <Container text style={{ marginTop: "10rem" }}>
+      <input
+        type="file"
+        accept=".pdf, .csv"
+        onChange={(e) => {
+          const { files } = e.target;
+
+          const fileAdded = files[0];
+          setMedia(fileAdded);
+
+          // I am splitting the name of file added by user to get extension..
+          // split will convert the name into an array..
+          const splitName = fileAdded.name.split(".");
+
+          // The extension will be the last element inside the array thats why I'm using splitName[splitName.length-1]
+          const fileExtension = splitName[splitName.length - 1];
+
+          setExtension(fileExtension);
+        }}
+      />
+
+      {/* Divider just to seperate the components */}
+      <Divider />
+
+      {media !== null && (
+        <>
+          <Icon
+            size="large"
+            name={
+              extension === "pdf"
+                ? "file pdf"
+                : extension === "csv" && "file excel"
+            }
+          />
+          <p>{media.name}</p>
+        </>
+      )}
+    </Container>
+  );
+}
