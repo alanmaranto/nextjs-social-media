aa74df649eba54429d9b27ef8fa98ce7e026beb1
alan <alanarostegui@hotmail.com>
2021-11-21 16:17:24 -0600

    feat: how getInitialProps works

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,23 @@ import Layout from "../components/Layout/Layout";
 import "semantic-ui-css/semantic.min.css";
 
 class MyApp extends App {
+  static async getInitialProps(appContext) {
+    const { Component, ctx } = appContext;
+
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return {
+      pageProps,
+    };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, pageProps } = this.props;
     return (
-      <Layout>
-        <Component />
+      <Layout {...pageProps}>
+        <Component {...pageProps} />
       </Layout>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import axios from "axios";
+
+const HomePage = ({ posts }) => {
+  return (
+    <div>
+      {posts &&
+        posts.length > 0 &&
+        posts.map((post) => <h1 key={post._id}>{post.title}</h1>)}
+    </div>
+  );
+};
+
+HomePage.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const { name } = ctx.query
+    console.log(name)
+    return {
+      posts: res.data,
+    };
+  } catch (error) {
+    return {
+      errorLoading: true,
+    };
+  }
 };
 
 export default HomePage;
